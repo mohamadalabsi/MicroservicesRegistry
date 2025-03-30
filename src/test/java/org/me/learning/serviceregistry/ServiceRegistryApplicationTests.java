d20244c2d735b85d7840bac326a7fc17c1120bc7
mohamadalabsi <absimoda1@gmail.com>
2025-03-30 22:44:19 +0200

    finishing service registry

---
+++ src/test/java/org/me/learning/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
